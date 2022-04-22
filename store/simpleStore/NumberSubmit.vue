7c832b3d5d72a32995f0b977ac7d4507c85fdfe4
aumartines <acm_asd@hotmail.com>
2022-04-22 03:10:41 -0600

    vue-stack

---
+++ store/simpleStore/NumberSubmit.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="newNumber" type="number" />
+    <button v-on:click="pushNewNumber(newNumber)">
+      Add new number
+    </button>
+  </div>
+</template>
+
+<script>
+import {store} from './store.js';
+
+export default {
+  name: 'NumberSubmit',
+  data () : {
+    return {
+      newNumber: 0,
+    }
+  },
+  methods: {
+    pushNewNumber(newNumber) {
+      store.pushNewNumber(newNumber);
+    }
+  }
+}
+
+</script>
---
+++ store/simpleStore/store.js
@@ -0,0 +1,8 @@
+export const store = {
+  state: {
+    numbers: [1,2,3],
+  },
+  pushNewNumber(newNumberString){
+    this.state.numbers.push(Number(newNumberString));
+  }
+}
